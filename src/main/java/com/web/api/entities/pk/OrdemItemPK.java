1db49239cf8c8a436152e0a24dd6b9017113cafe
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-05-31 15:26:57 -0300

    OrderItem, many-to-many association with extra attributes PART 1

---
+++ src/main/java/com/web/api/entities/pk/OrdemItemPK.java
@@ -0,0 +1,64 @@
+package com.web.api.entities.pk;
+
+import java.util.Objects;
+
+import com.web.api.entities.Order;
+import com.web.api.entities.Product;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class OrdemItemPK {
+
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	public OrdemItemPK() {
+	}
+
+	public OrdemItemPK(Order order, Product product) {
+		this.order = order;
+		this.product = product;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(order, product);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdemItemPK other = (OrdemItemPK) obj;
+		return Objects.equals(order, other.order) && Objects.equals(product, other.product);
+	}
+
+}
