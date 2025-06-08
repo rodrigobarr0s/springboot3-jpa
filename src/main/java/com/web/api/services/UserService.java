d90d6280e49b14b4f652a454e90c3fd453b01af8
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-06-08 09:57:14 -0300

    Exception handling - update

--- src/main/java/com/web/api/services/UserService.java
+++ src/main/java/com/web/api/services/UserService.java
@@ -45,9 +45,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
